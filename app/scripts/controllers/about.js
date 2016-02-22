99835855fa61e2fd32ff2ae598c22af6753fdf56
Wenchao Li <wenchao.li@citrix.com>
2016-02-22 12:16:28 +0800

    Project set up.

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uitestApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the uitestApp
+ */
+angular.module('uitestApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name uitestApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the uitestApp
+ */
+angular.module('uitestApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
